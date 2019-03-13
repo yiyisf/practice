6c43dd4f19623be9369231e355a008f7007a7c3f
sandra <yiyi119120@gmail.com>
2019-03-13 16:59:37 +0800

    add case

--- src/test/java/Base_Type.java
+++ src/test/java/Base_Type.java
@@ -28,11 +28,53 @@ public class Base_Type {
     @Test
     public void fun3(){
         int a = 3;
-        int b = 4;
         System.out.println("++a="+(++a));
     }
 
-    
+    @Test
+    public void fun4(){
+        int a = 11;
+        if (a>10){
+            System.out.println("a是大于10的");}
+        else {
+            System.out.println("a是小于10的");}
+        }
+    @Test
+    public void fun5(){
+        int a = -11;
+        if(a>10){
+            System.out.println("a是大于10的");
+        }else if (a==10){
+            System.out.println("a是小于10的");
+        }else if(a>0&&a<10){
+            System.out.println("a是小于10&&大于0的");
+        }else {
+            System.out.println("a是小于0的");
+        }
+    }
+
+    @Test
+    public void fun6(){
+        int a = 11;
+        int b = -20;
+        if (a>0 | b>0){
+            System.out.println("a或者B总有一个大于0的");}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
