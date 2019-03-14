c01b829181f6b9cc4cf952b85c8716157a6f62ef
sandra <yiyi119120@gmail.com>
2019-03-14 16:27:22 +0800

    add case

--- src/test/java/FrogTest.java
+++ src/test/java/FrogTest.java
@@ -5,26 +5,21 @@ import org.junit.Test;
 //        f(2)=2;
 //        f(n)=f(n-1)+f(n-2) n>=2;
 public class FrogTest {
-//    public int m =0;
-//    public int n=0;
 
     @Test
-    public int fun() {
-        int a;
+    public int fun(int a) {
         if (a <= 0) {
             return 0;
-//            System.out.println(0);
         } else if (a == 1 || a == 2) {
             return a;
-//            System.out.println(a);
         } else {
-//            m = fun(a-1);
-//            n = fun(a-2);
             return fun(a-1)+fun(a-2);
-//            System.out.println(fun(a-1));
 
         }
 
+    }
+    fun(int a){
+        return;
     }
 
 
