cd296cf7fc4f360dbe404069cdcb22f94fda5c45
liuzhe <779402401@qq.com>
2019-03-17 22:46:52 +0800

    第一章

--- src/main/java/pig/Pig.java
+++ src/main/java/pig/Pig.java
@@ -4,19 +4,19 @@ package pig;
  * Created by liuzhe on 2019/3/17.
  */
 public class Pig {
-    
-    String name ;
+
+    public String name ;
 
     public Pig(String name) {
         this.name = name;
     }
 
 
-    
 
-    public void eat(){
+
+    void eat(){
         
-        System.out.printf("八戒最爱吃馒头了")；
+        System.out.printf("八戒最爱吃馒头了");
 
     }
 }
