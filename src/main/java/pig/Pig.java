7b06e09c250fd7f99d0fb5a6e2a29012076c2103
liuzhe <779402401@qq.com>
2019-03-17 22:39:45 +0800

    第一章

---
+++ src/main/java/pig/Pig.java
@@ -0,0 +1,22 @@
+package pig;
+
+/**
+ * Created by liuzhe on 2019/3/17.
+ */
+public class Pig {
+    
+    String name ;
+
+    public Pig(String name) {
+        this.name = name;
+    }
+
+
+    
+
+    public void eat(){
+        
+        System.out.printf("八戒最爱吃馒头了")；
+
+    }
+}
---
+++ src/main/java/pig/PigTest.java
@@ -0,0 +1,14 @@
+package pig;
+
+/**
+ * Created by liuzhe on 2019/3/17.
+ */
+public class PigTest {
+
+
+    public static void main(String[] args) {
+        Pig pig = new Pig("猪八戒");
+        pig.eat();
+
+    }
+}
