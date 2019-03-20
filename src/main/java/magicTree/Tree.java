2c14d591537a6900fd6439ec0338fa13b457e38b
liuzhe <779402401@qq.com>
2019-03-20 23:16:08 +0800

    第si章

---
+++ src/main/java/magicTree/Tree.java
@@ -0,0 +1,43 @@
+package magicTree;
+
+/**
+ * Created by liuzhe on 2019/3/20.
+ */
+public class Tree {
+    private Integer total;
+    private Integer hideNum;
+
+    public Tree() {
+        this.total = 28;
+        this.hideNum = 0;
+    }
+
+    public void hide(Integer i) {
+        if (this.total - i >= 0) {
+            this.total = this.total - i;
+            this.hideNum = this.hideNum + i;
+            System.out.println("掉土里" + i + "个！");
+        } else {
+            System.out.println("树空了，没得吃了！");
+        }
+    }
+
+
+    public void recover(Integer i) {
+        this.total += i;
+        this.hideNum -= i;
+        System.out.println("恢复了" + i + "个，现在有" + total + "个了!");
+    }
+
+    public void beEaten(int i) {
+        total = total - i;
+        System.out.println("吃掉了" + i + "个");
+    }
+
+
+    public void hideAll() {
+        this.hideNum += this.total;
+        this.total = 0;
+        System.out.println("树上空了，土里有"+ this.hideNum+"个！");
+    }
+}
---
+++ src/main/java/magicTree/TreeTest.java
@@ -0,0 +1,25 @@
+package magicTree;
+
+/**
+ * Created by liuzhe on 2019/3/20.
+ */
+public class TreeTest {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.beEaten(1);//吃掉一个
+
+        tree.beEaten(1);
+
+        tree.hide(1);
+
+        tree.beEaten(3);
+
+        tree.hideAll();
+
+        tree.recover(23);
+
+    }
+
+}
