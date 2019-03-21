4c95f39929d6fc78500027d63989108784af20b3
liuzhe <779402401@qq.com>
2019-03-21 21:53:20 +0800

    第五章

---
+++ src/main/java/robot/Robot.java
@@ -0,0 +1,49 @@
+package robot;
+
+/**
+ * Created by liuzhe on 2019/3/21.
+ */
+public class Robot {
+    public boolean isCheaper(double price1,double price2){
+        return price1<price2;
+
+
+    }
+
+    public double totalAmount(double price,double amount){
+        return price*amount;
+
+    }
+    public boolean isYear(int year){
+        if ((year%4==0)&&(year%100!=0)||(year%400==0)){
+            return true;
+        }
+        else
+            return false;
+
+    }
+
+    public int encrypt(int data){
+//        %求余数  /求商
+// 9876   6  7 8 9
+
+        int data1=data%10;
+        int data2=data/10%10;
+        int data3=data/100%10;
+        int data4=data/1000;
+
+        data1=(data1+5)%10;
+        data2=(data1+5)%10;
+        data3=(data1+5)%10;
+        data4=(data1+5)%10;
+
+        int result = data1*1000+data2*10000+data3*100+data4*10;
+        return result;
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/robot/RobotTest.java
@@ -0,0 +1,20 @@
+package robot;
+
+/**
+ * Created by liuzhe on 2019/3/21.
+ */
+public class RobotTest {
+
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.isCheaper(3.4,4.5);
+        robot.totalAmount(4.45333,3);
+        robot.isYear(2019);
+        robot.encrypt(1234);
+
+        System.out.printf(String.valueOf(robot.isCheaper(3.4,4.5)));
+        System.out.printf(String.valueOf(robot.totalAmount(4.45333,3)));
+        System.out.printf(String.valueOf(robot.isYear(2019)));
+        System.out.printf(String.valueOf(robot.encrypt(1234)));
+    }
+}
