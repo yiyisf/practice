ead19f42f252f15db5648e550bd008d4bb3bc215
liuzhe <779402401@qq.com>
2019-03-21 21:57:58 +0800

    第五章

--- src/main/java/robot/RobotTest.java
+++ src/main/java/robot/RobotTest.java
@@ -12,9 +12,9 @@ public class RobotTest {
         robot.isYear(2019);
         robot.encrypt(1234);
 
-        System.out.printf(String.valueOf(robot.isCheaper(3.4,4.5)));
-        System.out.printf(String.valueOf(robot.totalAmount(4.45333,3)));
-        System.out.printf(String.valueOf(robot.isYear(2019)));
-        System.out.printf(String.valueOf(robot.encrypt(1234)));
+        System.out.printf(String.valueOf(robot.isCheaper(3.4,4.5))+'\n');
+        System.out.printf(String.valueOf(robot.totalAmount(4.45333,3))+'\n');
+        System.out.printf(String.valueOf(robot.isYear(2019))+'\n');
+        System.out.printf(String.valueOf(robot.encrypt(1234))+'\n');
     }
 }
