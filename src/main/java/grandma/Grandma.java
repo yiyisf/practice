08c04224e131b4023ad32bb22558055cdbf5b457
sandra <yiyi119120@gmail.com>
2019-03-21 19:11:09 +0800

    加强练习

--- src/main/java/grandma/Grandma.java
+++ src/main/java/grandma/Grandma.java
@@ -7,7 +7,7 @@ public class Grandma {
     public Grandma(String name, int age) {
         this.name = name;
         this.age = age;
-        if(this.age>=0){
+        if(this.age>=0&&this.age<120){
             System.out.println(this.name+"今年"+this.age+"岁了！");
         }else {
             System.out.println("年龄输入错误，请重新输入！");
