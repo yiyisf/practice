a9c9ab96f6f7929d8481a96de31b56101c20cdfb
liuzhe <779402401@qq.com>
2019-03-22 20:47:33 +0800

    加强练习

--- src/main/java/rabbit/Rabbit.java
+++ src/main/java/rabbit/Rabbit.java
@@ -6,17 +6,21 @@ public class Rabbit {
 
     public int Rabbit(int num) {
         this.num = num;
-        if (this.num == 2 || this.num == 1)
+        if (this.num == 1 || this.num == 2)
         {
             return 1;
         }
         else {
             return Rabbit(num - 1) + Rabbit(num - 2);
-
         }
 
 
     }
+
+//    public void Print(){
+//        System.out.printf("第"+this.num+"个月共有"+String.valueOf(Rabbit(this.num))+"只小兔子");
+//    }
+
 
 
 }
--- src/main/java/rabbit/RabbitTest.java
+++ src/main/java/rabbit/RabbitTest.java
@@ -5,8 +5,10 @@ public class RabbitTest {
 
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();
+//        rabbit.Rabbit(3);
 
-        System.out.printf(String.valueOf(rabbit.Rabbit(6)));
+        System.out.printf(String.valueOf(rabbit.Rabbit(4)));
+//        rabbit.Print();
 
 
 
